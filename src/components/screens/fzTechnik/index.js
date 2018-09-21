93b53fb8ce8230afc5c5232b4925bdd10699f443
Wenge <domenik.wenghofer@gmail.com>
2018-09-21 13:57:14 +0200

    added redux

--- src/components/screens/fzTechnik/index.js
+++ src/components/screens/fzTechnik/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+
 import GallerySection from '../../reusable/gallery';
 
+
 class FzTechnikPage extends Component {
+
   render() {
     return (
       <div className="container-fullWidth">
-        <GallerySection />
+        <GallerySection name="Fahrzeugtechnik"/>
       </div>
     );
   }
