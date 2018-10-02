fc64afb1977999897f4ea1089a50a6a575c1dd58
Elias <wuggenig1@gmail.com>
2018-10-02 15:52:28 +0200

    almost done preorder

--- src/components/reusable/preorder/index.js
+++ src/components/reusable/preorder/index.js
@@ -1,32 +1,57 @@
 import React, { Component } from 'react';
-import Popup from './popup';
+import { PopUp } from './popup';
 
 class Preorder extends Component{
-  title="Jetzt Vorbestellen";
-  text="der Text";
-
-  handleClick() {
-    const one = 1;
-    if ( one == 2 ) {
-      window.open("mailto:xyz@abc.com");
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
     }
-    else {
-      <Popup />
+  }
+
+  title="Jetzt Vorbestellen";
+  text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, a nostrum. Dolorem, repellat quidem ut, minima sint vel eveniet quibusdam voluptates delectus doloremque, explicabo tempore dicta adipisci fugit amet dignissimos?";
+
+  handleClick = () => {
+    var date = new Date();
+    var day = date.getDay();
+    var hours = date.getHours();
+
+     if ( day >= 1 &&  day <= 5 && hours >= 16 && hours <= 17){
+       window.location.href = "mailto:mail@example.org";
+     }
+     else {
+       this.setState({
+         open: !this.state.open,
+       })
+     }
+  }
+
+  togglePopUp = () => {
+    if (this.state.open === true) {
+      return <PopUp />
     }
   }
 
   render(){
     return(
-      <div className="row container">
-        <h1 className="light align-center title">
-          {this.title}
-        </h1>
-        <p className="text">
-          {this.text}
-        </p>
-        <button onClick={this.handleClick} className="btn-border-rounded-full">
-          jetzt Bestellen
-        </button>
+      <div className="container-middle-big">
+            <h1 className="light align-center">
+                {this.title}
+            </h1>
+
+        <div className="row container">
+            <p className="text align-center">
+              {this.text}
+            </p>
+        </div>
+
+        <div className="row container justify-center">
+            <button onClick={this.handleClick} className="btn-border-rounded-full orderButton">
+              jetzt Bestellen
+            </button>
+          {this.togglePopUp()}
+        </div>
       </div>
     );
   }
--- src/components/reusable/preorder/popup.js
+++ src/components/reusable/preorder/popup.js
@@ -1,47 +1,21 @@
 import React from "react";
-import Popup from "reactjs-popup";
 
-export default () => (
-  <Popup trigger={<button className="button"> Open Modal </button>} modal>
-    {close => (
-      <div className="modal">
-        <a className="close" onClick={close}>
-          &times;
-        </a>
-        <div className="header"> Modal Title </div>
-        <div className="content">
-          {" "}
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, a nostrum.
-          Dolorem, repellat quidem ut, minima sint vel eveniet quibusdam voluptates
-          delectus doloremque, explicabo tempore dicta adipisci fugit amet dignissimos?
-          <br />
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sit
-          commodi beatae optio voluptatum sed eius cumque, delectus saepe repudiandae
-          explicabo nemo nam libero ad, doloribus, voluptas rem alias. Vitae?
+export const PopUp = (props) => (
+  <div>
+    <div className="popupOverlay-close" onClick={(state) => this.setState({ open: false, })}></div>
+    <div className="popupOverlay">
+      <div className=" flexbox container-middle-small flex-align-center modal">
+        {/* <div className="close"></div> */}
+        <div className="popupContent">
+          <h5 className="light">Wichtige Information</h5>
+          <p className="content">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, a nostrum.
+            Dolorem, repellat quidem ut, minima sint vel eveniet quibusdam voluptates
+            delectus doloremque, explicabo tempore dicta adipisci fugit amet dignissimos?
+          </p>
         </div>
-        <div className="actions">
-          <Popup
-            trigger={<button className="button"> Trigger </button>}
-            position="top center"
-            closeOnDocumentClick
-          >
-            <span>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae magni omnis delectus
-              nemo, maxime molestiae dolorem numquam mollitia, voluptate ea, accusamus excepturi
-              deleniti ratione sapiente! Laudantium, aperiam doloribus. Odit, aut.
-            </span>
-          </Popup>
-          <button
-            className="button"
-            onClick={() => {
-              console.log('modal closed ')
-              close()
-            }}
-          >
-            close modal
-          </button>
       </div>
-      </div>
-    )}
-  </Popup>
+    </div>
+  </div>
+
 );
