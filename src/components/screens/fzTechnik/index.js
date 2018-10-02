ceb93b04ca78d4e5de3c005ba2b7a0dfca895062
Wenge <domenik.wenghofer@gmail.com>
2018-10-02 07:43:45 +0200

    added Svg to animate FZ-Technik page header

--- src/components/screens/fzTechnik/index.js
+++ src/components/screens/fzTechnik/index.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { ServiceList } from './service';
 import { ZahlungsModalitäten } from './zahlungen';
+import { Svg } from './svg';
+
+import SVG from '../../../media/grafiken/fz-technik.svg';
 
 class FzTechnikPage extends Component {
-
   render() {
     return (
       <div className="container-fullWidth">
+        <Svg />
         <ServiceList />
         <ZahlungsModalitäten />
       </div>
